ddde94cbf44e88d4c40a740e05a4a4dc98bf6856
abhishah1608 <abhishah1608@gmail.com>
2023-08-12 23:32:36 -0400

    initial commit

---
+++ Utility/autocomplete_sentence.py
@@ -0,0 +1,40 @@
+import openai
+
+class AutcompleteSentence:
+    sentence, context = None, None
+
+    # here is the API key for OpenAI.
+    openai.api_key = ''
+
+    def __init__(self, sentence, context):
+        self.sentence, self.context = sentence, context
+
+    def get_autcompleteSentence(self):
+        prompt = self.sentence
+
+        context = self.context
+
+        p_c = None
+
+        if context is None:
+            p_c = prompt
+        else:
+            p_c = context + '\n' + prompt
+
+        stop_token = ". "
+
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=p_c,
+            max_tokens=50,  # Adjust the number of tokens as needed
+            stop=stop_token,
+            temperature=0.9
+        )
+
+        translated_text = response.choices[0].text.strip()
+        return translated_text
+
+
+
+
+
---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template
+
+from Utility.autocomplete_sentence import AutcompleteSentence
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def openchatbot():  # put application's code here
+    return render_template('chatbot.html')
+
+@app.route('/auto-complete', methods=['POST'])
+def translate():
+    body = request.get_json()
+    sentence = body.get('sentence')
+    context =  body.get('context')
+    t = AutcompleteSentence(sentence,context)
+    response = t.get_autcompleteSentence()
+    return response
+
+
+if __name__ == '__main__':
+    app.run()
